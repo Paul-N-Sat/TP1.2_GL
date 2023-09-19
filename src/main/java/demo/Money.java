6837f9bf00cad1cae1b174b52873b162a428e216
p_scheller <pschelle@enssat.fr>
2023-09-19 18:08:46 +0200

    Test Approuved

--- src/main/java/demo/Money.java
+++ src/main/java/demo/Money.java
@@ -1,4 +1,37 @@
 package demo;
 
+import java.util.Objects;
+
 public class Money {
+    int amount;
+    String currency;
+
+    public Money(int amount, String currency){
+        this.currency=currency;
+        this.amount=amount;
+    }
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", devise='" + currency + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return amount == money.amount && Objects.equals(currency, money.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount, currency);
+    }
+
+    public Money add(Money that) {
+        return new Money(this.amount+that.amount,this.currency);
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import demo.Money;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Money lesSous = new Money(42,"EUR");
+        System.out.println(lesSous);
     }
 }
